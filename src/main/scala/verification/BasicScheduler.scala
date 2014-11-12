f527bb07f7ab13af89e41c09f0719d419b083819
Aurojit <apanda@cs.berkeley.edu>
2014-11-11 18:50:24 -0800

    Initial commit based on Vjeko's modifications

---
+++ src/main/scala/verification/BasicScheduler.scala
@@ -0,0 +1,206 @@
+package akka.dispatch.verification
+
+import akka.actor.ActorCell
+import akka.actor.ActorSystem
+import akka.actor.ActorRef
+import akka.actor.Actor
+import akka.actor.PoisonPill
+import akka.actor.Props;
+
+import akka.dispatch.Envelope
+import akka.dispatch.MessageQueue
+import akka.dispatch.MessageDispatcher
+
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable.Queue
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
+import scala.collection.Iterator
+
+import scala.collection.generic.GenericTraversableTemplate
+
+// A basic scheduler
+class BasicScheduler extends Scheduler {
+  
+  var intrumenter = Instrumenter
+  var currentTime = 0
+  var index = 0
+  
+  type CurrentTimeQueueT = Queue[Event]
+  
+  var currentlyProduced = new CurrentTimeQueueT
+  var currentlyConsumed = new CurrentTimeQueueT
+  
+  var producedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+  var consumedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+  
+  var prevProducedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+  var prevConsumedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+ 
+  // Current set of enabled events.
+  val pendingEvents = new HashMap[String, Queue[(ActorCell, Envelope)]]  
+
+  val actorNames = new HashSet[String]
+  
+  
+  // Is this message a system message
+  def isSystemMessage(src: String, dst: String): Boolean = {
+    if ((actorNames contains src) || (actorNames contains dst))
+      return false
+    
+    return true
+  }
+  
+  
+  // Notification that the system has been reset
+  def start_trace() : Unit = {
+    prevProducedEvents = producedEvents
+    prevConsumedEvents = consumedEvents
+    producedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+    consumedEvents = new Queue[ (Integer, CurrentTimeQueueT) ]
+  }
+  
+  
+  // When executing a trace, find the next trace event.
+  private[this] def mutable_trace_iterator(
+      trace: Queue[ (Integer, CurrentTimeQueueT) ]) : Option[Event] = { 
+    
+    if(trace.isEmpty) return None
+      
+    val (count, q) = trace.head
+    q.isEmpty match {
+      case true =>
+        trace.dequeue()
+        mutable_trace_iterator(trace)
+      case false => return Some(q.dequeue())
+    }
+  }
+  
+  
+
+  // Get next message event from the trace.
+  private[this] def get_next_trace_message() : Option[MsgEvent] = {
+    mutable_trace_iterator(prevConsumedEvents) match {
+      case Some(v : MsgEvent) =>  Some(v)
+      case Some(v : Event) => get_next_trace_message()
+      case None => None
+    }
+  }
+  
+  
+  
+  // Figure out what is the next message to schedule.
+  def schedule_new_message() : Option[(ActorCell, Envelope)] = {
+  
+    // Filter for messages belong to a particular actor.
+    def is_the_same(e: MsgEvent, c: (ActorCell, Envelope)) : Boolean = {
+      val (cell, env) = c
+      e.receiver == cell.self.path.name
+    }
+
+    // Get from the current set of pending events.
+    def get_pending_event()  : Option[(ActorCell, Envelope)] = {
+      // Do we have some pending events
+      pendingEvents.headOption match {
+        case Some((receiver, queue)) =>
+           if (queue.isEmpty == true) {
+             
+             pendingEvents.remove(receiver) match {
+               case Some(key) => schedule_new_message()
+               case None => throw new Exception("internal error")
+             }
+             
+           } else {
+              Some(queue.dequeue())
+           }
+        case None => None
+      }
+    }
+    
+    get_next_trace_message() match {
+     // The trace says there is something to run.
+     case Some(msg_event : MsgEvent) => 
+       pendingEvents.get(msg_event.receiver) match {
+         case Some(queue) => queue.dequeueFirst(is_the_same(msg_event, _))
+         case None => None
+       }
+     // The trace says there is nothing to run so we have either exhausted our
+     // trace or are running for the first time. Use any enabled transitions.
+     case None => get_pending_event()
+       
+   }
+  }
+  
+  
+  // Get next event
+  def next_event() : Event = {
+    mutable_trace_iterator(prevConsumedEvents) match {
+      case Some(v) => v
+      case None => throw new Exception("no previously consumed events")
+    }
+  }
+  
+
+  // Record that an event was consumed
+  def event_consumed(event: Event) = {
+    currentlyConsumed.enqueue(event)
+  }
+  
+  
+  def event_consumed(cell: ActorCell, envelope: Envelope) = {
+    currentlyConsumed.enqueue(new MsgEvent(
+        envelope.sender.path.name, cell.self.path.name, 
+        envelope.message, cell, envelope))
+  }
+  
+  // Record that an event was produced 
+  def event_produced2(event: SpawnEvent) = {
+    currentlyProduced.enqueue(event)
+  }
+  
+  // Record that an event was produced 
+  def event_produced(event: Event) = {
+    event match {
+      case event : SpawnEvent => actorNames += event.name
+    }
+    currentlyProduced.enqueue(event)
+  }
+  
+  
+  def event_produced(cell: ActorCell, envelope: Envelope) = {
+    val snd = envelope.sender.path.name
+    val rcv = cell.self.path.name
+    val msgs = pendingEvents.getOrElse(rcv, new Queue[(ActorCell, Envelope)])
+    
+    pendingEvents(rcv) = msgs += ((cell, envelope))
+    currentlyProduced.enqueue(new MsgEvent(snd, rcv, envelope.message, cell, envelope))
+  }
+  
+  
+  // Called before we start processing a newly received event
+  def before_receive(cell: ActorCell) {
+    producedEvents.enqueue( (currentTime, currentlyProduced) )
+    consumedEvents.enqueue( (currentTime, currentlyConsumed) )
+    currentlyProduced = new CurrentTimeQueueT
+    currentlyConsumed = new CurrentTimeQueueT
+    currentTime += 1
+    println(Console.GREEN 
+        + " ↓↓↓↓↓↓↓↓↓ ⌚  " + currentTime + " | " + cell.self.path.name + " ↓↓↓↓↓↓↓↓↓ " + 
+        Console.RESET)
+  }
+  
+  
+  // Called after receive is done being processed 
+  def after_receive(cell: ActorCell) {
+    println(Console.RED 
+        + " ↑↑↑↑↑↑↑↑↑ ⌚  " + currentTime + " | " + cell.self.path.name + " ↑↑↑↑↑↑↑↑↑ " 
+        + Console.RESET)
+        
+  }
+  
+
+  def notify_quiescence () {
+  }
+  
+
+}
---
+++ src/main/scala/verification/Instrumenter.scala
@@ -0,0 +1,232 @@
+package akka.dispatch.verification
+
+import akka.actor.ActorCell
+import akka.actor.ActorSystem
+import akka.actor.ActorRef
+import akka.actor.Actor
+import akka.actor.PoisonPill
+import akka.actor.Props;
+
+import akka.dispatch.Envelope
+import akka.dispatch.MessageQueue
+import akka.dispatch.MessageDispatcher
+
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable.Queue
+import scala.collection.mutable.Stack
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
+import scala.util.control.Breaks
+
+abstract class Event
+
+case class MsgEvent(sender: String, receiver: String, msg: Any, 
+               cell: ActorCell, envelope: Envelope) extends Event
+
+case class SpawnEvent(parent: String,
+    props: Props, name: String, actor: ActorRef) extends Event
+
+class Instrumenter {
+
+  var scheduler : Scheduler = new NullScheduler
+  val dispatchers = new HashMap[ActorRef, MessageDispatcher]
+  
+  val allowedEvents = new HashSet[(ActorCell, Envelope)]  
+  
+  val seenActors = new HashSet[(ActorSystem, Any)]
+  val actorMappings = new HashMap[String, ActorRef]
+  
+  // Track the executing context (i.e., source of events)
+  var currentActor = ""
+  var counter = 0   
+  var started = false;
+  
+  // Callbacks for new actors being created
+  def new_actor(system: ActorSystem, 
+      props: Props, name: String, actor: ActorRef) : Unit = {
+    
+    val event = new SpawnEvent(currentActor, props, name, actor)
+    scheduler.event_produced(event : SpawnEvent)
+    scheduler.event_consumed(event)
+
+    if (!started) {
+      seenActors += ((system, (actor, props, name)))
+    }
+    
+    actorMappings(name) = actor
+      
+    println("System has created a new actor: " + actor.path.name)
+  }
+  
+  def new_actor(system: ActorSystem, 
+      props: Props, actor: ActorRef) : Unit = {
+    new_actor(system, props, actor.path.name, actor)
+  }
+  
+  
+  // Restart the system:
+  //  - Create a new actor system
+  //  - Inform the scheduler that things have been reset
+  //  - Run the first event to start the first actor
+  //  - Send the first message received by this actor.
+  //  This is all assuming that we don't control replay of main
+  //  so this is a way to replay the first message that started it all.
+  def reinitialize_system(sys: ActorSystem, argQueue: Queue[Any]) {
+    require(scheduler != null)
+    val newSystem = ActorSystem("new-system-" + counter)
+    counter += 1
+    println("Started a new actor system.")
+
+    // Tell scheduler that we are done restarting and it should prepare
+    // to start the system
+    // TODO: This should probably take sys as an argument or something
+    scheduler.start_trace()
+    
+    // We expect the first event to be an actor spawn (not actors exist, nothing
+    // to run).
+    val first_spawn = scheduler.next_event() match {
+      case e: SpawnEvent => e
+      case _ => throw new Exception("not a spawn")
+    }
+    
+    // Start the actor using a new actor system. (All subsequent actors
+    // are expected to spawn from here, so they will automatically inherit
+    // the new actor system)
+    for (args <- argQueue) {
+      args match {
+        case (actor: ActorRef, props: Props, first_spawn.name) =>
+          println("starting " + first_spawn.name)
+          newSystem.actorOf(props, first_spawn.name)
+      }
+    }
+
+    // TODO: Maybe we should do this differently (the same way we inject external
+    // events, etc.)
+    // Kick off the system by replaying a message
+    val first_msg = scheduler.next_event() match {
+      case e: MsgEvent => e
+      case _ => throw new Exception("not a message")
+    }
+    
+    actorMappings.get(first_msg.receiver) match {
+      case Some(ref) => ref ! first_msg.msg
+      case None => throw new Exception("no such actor " + first_msg.receiver)
+    }
+  }
+  
+  // Signal to the instrumenter that the scheduler wants to restart the system
+  def restart_system() = {
+    
+    println("Restarting system")
+    started = false
+    
+    val allSystems = new HashMap[ActorSystem, Queue[Any]]
+    for ((system, args) <- seenActors) {
+      val argQueue = allSystems.getOrElse(system, new Queue[Any])
+      argQueue.enqueue(args)
+      allSystems(system) = argQueue
+    }
+
+    seenActors.clear()
+    for ((system, argQueue) <- allSystems) {
+        println("Shutting down the actor system. " + argQueue.size)
+        system.shutdown()
+        system.registerOnTermination(reinitialize_system(system, argQueue))
+    }
+  }
+  
+  // Called before a message is received
+  def beforeMessageReceive(cell: ActorCell) {
+    
+    if (scheduler.isSystemMessage(cell.sender.path.name, cell.self.path.name)) return
+   
+    scheduler.before_receive(cell)
+    currentActor = cell.self.path.name
+  }
+  
+  // Called after the message receive is done.
+  def afterMessageReceive(cell: ActorCell) {
+    if (scheduler.isSystemMessage(cell.sender.path.name, cell.self.path.name)) return
+    
+    scheduler.after_receive(cell)          
+    scheduler.schedule_new_message() match {
+      case Some((new_cell, envelope)) => dispatch_new_message(new_cell, envelope)
+      case None =>
+        counter += 1
+        println("Nothing to run.")
+        started = false
+        scheduler.notify_quiescence()
+    }
+
+  }
+
+  // Dispatch a message, i.e., deliver it to the intended recipient
+  def dispatch_new_message(cell: ActorCell, envelope: Envelope) = {
+    val snd = envelope.sender.path.name
+    val rcv = cell.self.path.name
+    
+    allowedEvents += ((cell, envelope) : (ActorCell, Envelope))        
+
+    val dispatcher = dispatchers.get(cell.self) match {
+      case Some(value) => value
+      case None => throw new Exception("internal error")
+    }
+    
+    scheduler.event_consumed(cell, envelope)
+    dispatcher.dispatch(cell, envelope)
+  }
+  
+  
+  // Called when dispatch is called.
+  def aroundDispatch(dispatcher: MessageDispatcher, cell: ActorCell, 
+      envelope: Envelope): Boolean = {
+
+    val value: (ActorCell, Envelope) = (cell, envelope)
+    val receiver = cell.self
+    val snd = envelope.sender.path.name
+    val rcv = receiver.path.name
+    
+    // If this is a system message just let it through.
+    if (scheduler.isSystemMessage(snd, rcv)) { return true }
+    
+    // If this is not a system message then check if we have already recorded
+    // this event. Recorded => we are injecting this event (as opposed to some 
+    // actor doing it in which case we need to report it)
+    if (allowedEvents contains value) {
+      allowedEvents.remove(value) match {
+        case true => 
+          return true
+        case false => throw new Exception("internal error")
+      }
+    }
+    
+    // Record the dispatcher for the current receiver.
+    dispatchers(receiver) = dispatcher
+
+    // Have we started dispatching messages (i.e., is the loop in after_message_receive
+    // running?). If not then dispatch the current message and start the loop.
+    if (!started) {
+      started = true
+      dispatch_new_message(cell, envelope)
+      return false
+    }
+    
+    // Record that this event was produced
+    scheduler.event_produced(cell, envelope)
+    
+    println(Console.BLUE + "enqueue: " + snd + " -> " + rcv + Console.RESET);
+
+    return false
+  }
+
+}
+
+object Instrumenter {
+  var obj:Instrumenter = null
+  def apply() = {
+    if (obj == null) {
+      obj = new Instrumenter
+    }
+    obj
+  }
+}
