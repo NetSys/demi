24e1f6b8d45c23c123b2633385992b76dec9b3aa
colin-scott <cs@cs.berkeley.edu>
2015-09-02 15:16:24 -0700

    Add echo

--- tools/rerun_experiments.sh
+++ tools/rerun_experiments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+(cd experiments; git pull)
+
 for branch in raft-45 raft-46 raft-56 raft-58 raft-58-initialization raft-42 raft-66; do
+  echo "==================== Running $branch =================="
   git checkout $branch
   git pull
   sbt assembly && java -d64 -Xmx15g -cp target/scala-2.11/randomSearch-assembly-0.1.jar akka.dispatch.verification.Main > console.out
