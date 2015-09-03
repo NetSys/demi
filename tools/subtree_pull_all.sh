e5063f745f9f2ca189f606340dd9c05916c09547
colin-scott <cs@cs.berkeley.edu>
2015-09-02 20:47:50 -0700

    Option to run bash

--- tools/subtree_pull_all.sh
+++ tools/subtree_pull_all.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+RUN_BASH=$1
+
 for branch in raft-45 raft-46 raft-56 raft-58 raft-58-initialization raft-42 raft-66; do
   git checkout $branch
   git pull
   git subtree pull --prefix=interposition interposition master
+  if [ "$RUN_BASH" != "" ]; then
+    bash
+  fi
   git push
 done
