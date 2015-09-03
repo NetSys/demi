2d64e69311f60bca420ef8c090fcc487adea6cde
colin-scott <cs@cs.berkeley.edu>
2015-09-03 12:05:23 -0700

    Allow script to start at a certain point

--- tools/rerun_experiments.sh
+++ tools/rerun_experiments.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+START_WITH=$1
+
 (cd experiments; git pull)
 
 for branch in raft-45 raft-46 raft-56 raft-58 raft-58-initialization raft-42 raft-66; do
+  if [ "$START_WITH" != "" -a "$START_WITH" != $branch ]; then
+    continue
+  fi
+  if [ "$START_WITH" != "" -a "$START_WITH" == $branch ]; then
+    START_WITH=""
+  fi
+
   echo "==================== Running $branch =================="
   git checkout $branch
   git pull
